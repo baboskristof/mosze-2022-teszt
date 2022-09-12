5f9daff8b9bb225a1d006e954c30a6e9486a7b89
Babos Kristóf <babos.kristof@gmail.com>
2022-09-12 19:30:52 +0200

    All errors are commented, to be fixed

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; // constexpr helyett const kell
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //nincs ilyen változó, hogy NELEMENTS (csak N_ELEMENTS), illetve felesleges pointert használni
+    std::cout << '1-100 ertekek duplazasa' // idézőjelnek kell lennie stringnél (""), nem aposztrofnak (''), illetve ; hiányzik
+    for (int i = 0;) // nincs befejezve a for ciklus 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // for ciklus második paramétere hiányos
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ; hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nem elég deklarálni, mert később már fel akarjuk használni az értékét (ami nincs)
+    for (int i = 0; i < N_ELEMENTS, i++) // második és harmadik maraméter között nem , hanem ; kell
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
