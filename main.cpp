aa90fb968d29530f81bee14d6fb52129214bb3b8
Babos Kristóf <babos.kristof@gmail.com>
2022-09-12 19:41:27 +0200

    Errors fixed, working code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100; // constexpr helyett const kell
+const int N_ELEMENTS = 100; // constexpr helyett const kell
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //nincs ilyen változó, hogy NELEMENTS (csak N_ELEMENTS), illetve felesleges pointert használni
-    std::cout << '1-100 ertekek duplazasa' // idézőjelnek kell lennie stringnél (""), nem aposztrofnak (''), illetve ; hiányzik
-    for (int i = 0;) // nincs befejezve a for ciklus 
+    int b[N_ELEMENTS]; //nincs ilyen változó, hogy NELEMENTS (csak N_ELEMENTS), illetve felesleges pointert használni
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // idézőjelnek kell lennie stringnél (""), nem aposztrofnak (''), illetve ; hiányzik
+    for (int i = 0; i < N_ELEMENTS; i++) // nincs befejezve a for ciklus 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // for ciklus második paramétere hiányos
+    for (int i = 0; i < N_ELEMENTS; i++) // for ciklus második paramétere hiányos
     {
-        std::cout << "Ertek:" // ; hiányzik
+        std::cout << "Ertek: " << b[i] << std::endl; // ; hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag; // nem elég deklarálni, mert később már fel akarjuk használni az értékét (ami nincs)
-    for (int i = 0; i < N_ELEMENTS, i++) // második és harmadik maraméter között nem , hanem ; kell
+    for (int i = 0; i < N_ELEMENTS; i++) // második és harmadik maraméter között nem , hanem ; kell
     {
-        atlag += b[i] // ; hiányzik
+        atlag += b[i]; // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
